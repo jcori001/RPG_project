45dc56ccf4308dd4842affd6740be0bf9273b71f
jcori001 <76401655+jcori001@users.noreply.github.com>
2020-12-22 19:49:02 -0500

    Update inventory.py

--- classes/inventory.py
+++ classes/inventory.py
@@ -7,11 +7,11 @@ class Item:
 
 
 # Create Items
-potion = Item("Potion", "potion", "Heal 200 HP", 200)
-hipotion = Item("Hi-Potion", "potion", "Heals 500 HP", 500)
+potion = Item("Potion", "potion", "Heal 250 HP", 250)
+hipotion = Item("Hi-Potion", "potion", "Heals 600 HP", 600)
 
 elixir = Item("Elixir", "elixir", "Fully restores HP/HP of one party member", 9999)
 megaelixir = Item("Megaelixir", "elixir", "Fully restores party's HP/MP", 9999)
 
-grenade = Item("Grenade", "attack", "Deals 200 damage to one enemy", 200)
-h_bomb = Item("H-Bomb", "attack", "Deals 500 damage to one enemy", 500)
+grenade = Item("Grenade", "attack", "Deals 300 damage to one enemy", 300)
+big_one = Item("Big One", "attack", "Deals 600 damage to one enemy", 600)
