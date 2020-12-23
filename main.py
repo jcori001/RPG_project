cbfe5e705310232687e0f7725e2a80b0d50773a2
jcori001 <76401655+jcori001@users.noreply.github.com>
2020-12-22 19:47:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from classes.character_list import *
+from colorama import init
+init() #ANSI coloring support on cmd.exe
 
 
 '''
 NOTES:
--enemies are hard coded to have x4 magnitude heal
+-enemies are hard coded to have x3 magnitude heal
 -res is hard coded to work with revive only
 
-Future Mechanics:
--defense and magic defense math to reduce dmg
--speed damage through magic
--elemental weakness
--add special attacks
+Possible Upgrades
+-define crit roll method under class to delete repetetive dmg calculations
+-use defense and magic defense math to reduce dmg
+-enable speed damage through magic, elemental weakness
+-create special attacks
+-add flavor text for out of items
+-import characters/magics from csv/excel reads
 '''
 
 
@@ -31,7 +35,7 @@ while running:
             input("\nPress enter to continue...")
             
             #turn start stat changes
-            unit.manipulate_mp(int(round(unit.mp*0.03, 0)))
+            unit.manipulate_mp(int(round(unit.maxmp*0.05, 0)))
             #print status banner
             unit.banner(players, enemies)  
             #reset unit ATB after print
@@ -40,7 +44,9 @@ while running:
             if unit in players:
                 #run player loop
                 player_turn = True
-                print(bcolors.OKBLUE + f'\n\n{unit.name} charged {int(round(unit.mp*0.03, 0))} MP.' + bcolors.ENDC)
+                if unit.mp < unit.maxmp:
+                    print(bcolors.OKBLUE + f'\n\n{unit.name} charged {int(round(unit.maxmp*0.05, 0))} MP.' + bcolors.ENDC)
+                
                 while player_turn == True:
                     back = False
                     index = unit.choose_action()
@@ -158,6 +164,7 @@ while running:
                 #check if player won
                 if not enemies:
                     print(bcolors.OKGREEN + "\nYou win!" + bcolors.ENDC)
+                    input("\nPress enter to end the game...")
                     running = False
                 else:
                     continue
@@ -168,7 +175,7 @@ while running:
                 enemy_choice = random.randrange(0, 2) #no items coded into enemies
                 targetable_party = []
                 for player in players:
-                    if player.name not in fainted:
+                    if player not in fainted:
                         targetable_party.append(player)
             
                 if enemy_choice == 0:
@@ -184,7 +191,7 @@ while running:
                     if magic.type == "heal":
                         print(f'\n{unit.name} casts {magic.name}...')
                         magic_dmg = magic.generate_damage()
-                        unit.heal(magic_dmg*4) #set stronger heal for bosses
+                        unit.heal(magic_dmg*3) #set stronger heal for bosses
                         print(bcolors.FAIL + f'{magic.name} heals for {str(magic_dmg)} HP.' + bcolors.ENDC) 
                     elif magic.type == "dmg":
                         target = random.randrange(0, len(targetable_party))
@@ -210,6 +217,9 @@ while running:
                 if len(fainted) == len(players):
                     #print final banner
                     unit.banner(players, enemies)
+                    print (bcolors.FAIL + "\nYour enemies have defeated you!" + bcolors.ENDC)
+                    input("\nPress enter to end the game...")
+                    running = False
 
         else:
             if unit.hp > 0:
